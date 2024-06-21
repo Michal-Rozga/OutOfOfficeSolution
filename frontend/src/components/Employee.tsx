4feb5a2decadfc502d20705a623d58f1958f8969
Michal <michal.rozga@yahoo.com>
2024-06-22 01:06:59 +0200

    Created shell components for the task

---
+++ frontend/src/components/Employee.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Employee: React.FC = () => {
+  return (
+    <div>
+      <h3>Employee Component</h3>
+      <p>This component is visible to all users.</p>
+    </div>
+  );
+};
+
+export default Employee;
---
+++ frontend/src/components/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects: React.FC = () => {
+  return (
+    <div>
+      <h3>Projects Component</h3>
+      <p>This component is visible to all users.</p>
+    </div>
+  );
+};
+
+export default Projects;
